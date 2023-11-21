c8d49baad326920054f20b12324dab2c53847dd5
Rémi Bardenet <remi.bardenet@gmail.com>
2023-11-21 12:18:27 +0100

    trying to solve problem with publication query on HAL

--- getPublis.py
+++ getPublis.py
@@ -1,6 +1,8 @@
 import urllib.request
+
 page = urllib.request.urlopen("http://haltools.archives-ouvertes.fr/Public/afficheRequetePubli.php?auteur_exp=rbardenet&CB_auteur=oui&CB_titre=oui&CB_article=non&CB_Resume_court=non&CB_typdoc=oui&CB_vignette=non&langue=Anglais&tri_exp=annee_publi&tri_exp2=typdoc&tri_exp3=date_publi&&ordre_aff=TA&Fen=Aff&css=../css/VisuCondenseSsCadre.css")
 page = page.readlines()
+
 with open("publis.html",'wb') as fout:
     for line in page:
         strLine = line.decode()
@@ -46,4 +48,5 @@ with open("publis.html",'wb') as fout:
             checkStr = "P. Abreu"
             if length > 200 and checkStr in authorList:
                 strLine = strLine.replace(authorList, "The Full Pierre Auger collaboration</dd>")
+                
         fout.write(strLine.encode())
